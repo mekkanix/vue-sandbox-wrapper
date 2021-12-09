dba1418857930c0ef9ff94a8fd275fa9cb98c9f3
polo <paulguzdariviere@gmail.com>
2021-12-09 18:33:31 +0100

    Add JSDoc to PropPrimitiveValue

--- src/components/VSPrimitiveValue.vue
+++ src/components/VSPrimitiveValue.vue
@@ -7,13 +7,30 @@
 <script>
 import { formatPrimitiveValueToCode, } from '@/helpers/Formatter.js'
 
+/**
+ * VSPrimitiveValue
+ *
+ * Formatted displaying & colors of a primitive `value`, depending
+ * of its given `type`.
+ */
+
 export default {
   name: 'VSPrimitiveValue',
 
   props: {
+    /**
+     * Primitive (raw) value to display.
+     *
+     * @type {mixed}
+     */
     value: {
       type: [Boolean, String, Number, Date,],
     },
+    /**
+     * VS-formatted type of the primitive value.
+     *
+     * @type {string}
+     */
     type: {
       type: String,
       required: true,
@@ -22,11 +39,22 @@ export default {
   },
 
   computed: {
+    /**
+     * "Code"-formatted representation of the current primitive value.
+     *
+     * @return {string}
+     */
     codeFormattedValue () {
       // This is needed because passing an empty string "" to a prop makes Vue convert it to `true`
       const value = this.value === true && this.type === 'string' ? '' : this.value
       return formatPrimitiveValueToCode(value, this.type)
     },
+    /**
+     * Computed element's classes, applied depending on the current
+     * `type`.
+     *
+     * @return {object}
+     */
     cssClasses () {
       return {
         't-null': this.type === 'null',
--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/*!
+ * VueSandbox-wrapper v1.0.0-beta.1
+ * (c) 2021-2021 Mekkanix
+ * Released under the MIT License.
+ */
+
 import VSComponentWrapper from './components/VSComponentWrapper.vue'
 
 export default { VSComponentWrapper, }
